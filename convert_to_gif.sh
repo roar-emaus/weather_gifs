d4c6d722672d88513de26fe906f2f029a99180f7
Roar Emaus <roar@emaus.no>
2022-06-19 21:37:48 +0200

    added conversion command and get forecast data comman

---
+++ convert_to_gif.sh
@@ -0,0 +1,1 @@
+convert -delay 10 -loop 0 -resize 800x640 *.png out.gif
---
+++ data/get_latest_forecast.sh
@@ -0,0 +1,2 @@
+mv met_forecast_1_0km_nordic_latest.nc met_forecast_1_0km_nordic_latest.nc.backup
+wget https://thredds.met.no/thredds/fileServer/metpplatest/met_forecast_1_0km_nordic_latest.nc
