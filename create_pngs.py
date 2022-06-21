e4d469dc05bca8233326d8791c4427699ba8091a
Roar Emaus <roar@emaus.no>
2022-06-21 07:35:55 +0200

    WIP: working on span of range

--- create_pngs.py
+++ create_pngs.py
@@ -17,19 +17,23 @@ def open_dataset():
 def create_temperature_pngs():
     ds = open_dataset()
     air_t = ds.air_temperature_2m.to_numpy()
-    matrix = convert_to_rgb(air_t, spectral_r)
+    min_dist = 273.15 - air_t.min() if air_t.min() < 273.15 else 0.01
+    max_dist = air_t.max() - 273.15 if air_t.max() > 273.15 else 0.01
+    span = np.linspace(air_t.min(), air_t.max(), 254) 
+    
+    matrix = convert_to_rgb(air_t, span, spectral_r)
     save_to_png(matrix, name="air_temp")
 
 
 @jit(nopython=True)
-def convert_to_rgb(matrix, lut):
+def convert_to_rgb(matrix, span, lut):
     t_n, r_n, c_n = matrix.shape
-    air_range = np.linspace(233.14, 313.15, 254)
+    #air_range = np.linspace(233.14, 313.15, 254)
     result = np.zeros(matrix.shape + (3,))
     for t in range(t_n):
         for r in range(r_n):
             for c in range(c_n):
-                ind = int(np.argmax(air_range > matrix[t, r, c]))
+                ind = int(np.argmax(span > matrix[t, r, c]))
                 result[t, r, c] = lut[ind]
         print(t)
     return result
